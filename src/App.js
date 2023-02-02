5b0e96ed3d9570593a5de302d4c9b999e109f144
Colby Harper <colby.harper11@gmail.com>
2023-02-02 17:32:35 -0500

    add text area for printing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import './App.css';
+import React, { useId, useState } from 'react'
 
 const printNewSudoku = async () => {
   const result = await fetch('http://192.168.1.155:3000/sudoku', {
@@ -12,9 +13,38 @@ const printNewSudoku = async () => {
 }
 
 function App() {
+  const postTextAreaId = useId();
+  const [value, setValue] = useState();
+
+  const printText = async () => {
+    const my_data = { content: value};
+    const result = await fetch('http://192.168.1.155:3000/text', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(my_data)
+    });
+
+  console.log(result);
+  setValue("");
+  };
+
   return (
     <div>
       <button onClick={printNewSudoku}>Sudoku</button>
+      <label htmlFor={postTextAreaId}>
+        Add text to print:
+      </label>
+      <textarea
+        id={postTextAreaId}
+        name="postContent"
+        rows={4}
+        cols={40}
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <button onClick={printText}>Print</button>
     </div>
   )
 }
