7ae585cbb990206c5bd44e3ce6755f138d0491bc
Colby Harper <colby.harper11@gmail.com>
2023-02-02 16:34:07 -0500

    add sudoku button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+
+const printNewSudoku = async () => {
+  const result = await fetch('http://192.168.1.155:3000/sudoku', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+
+  console.log(result);
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={printNewSudoku}>Sudoku</button>
     </div>
-  );
+  )
 }
 
 export default App;
