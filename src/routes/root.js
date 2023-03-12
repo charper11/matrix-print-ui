c041a2da65cf5c32a583316a0ca0d661db3b4948
Colby Harper <colby.harper11@gmail.com>
2023-03-12 16:53:32 -0400

    style change

--- src/routes/root.js
+++ src/routes/root.js
@@ -5,24 +5,30 @@ export default function Root() {
   return (
     <>
       <div className="homepage">
-        <div className="main-content">
           <div className="navbar">
             <Link className="navbar-link" to={`custom-print`}>
               Custom print
             </Link>
-            <Link className="navbar-link" to={`link-print`}>
-              Link print
-            </Link>
             <Link className="navbar-link" to={`sudoku`}>
               Sudoku
+            </Link>
+            <Link className="navbar-link" to={`link-print`}>
+              Link print
             </Link>
             <Link className="navbar-link" to={`alarm`}>
               Alarm
             </Link>
           </div>
+          <div className="table-border">
           <div className="page-content">
+            <div className="print-table">
+              <span>Insta Print:</span>
+              <span className="print-option">newspaper</span>
+              <span className="print-option">perfectly imperfect</span>
+              <span className="print-option">morning brew</span>
+            </div>
           </div>
-        </div>
+          </div>
       </div>
     </>
   );
