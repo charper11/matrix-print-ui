91120225099f5ee01ce4171e99c4c3f89b0a6f04
Colby Harper <colby.harper11@gmail.com>
2023-03-14 14:07:16 -0400

    add options to sudoku print using react form

--- src/routes/sudoku.js
+++ src/routes/sudoku.js
@@ -1,17 +1,34 @@
-import React, { useState } from "react";
+import React from "react";
 import { PROGRAMS, print } from '../utils/printer-utils';
 
 export default function Sudoku() {
 
-  async function printSudoku() {
-    // for now, just print sudoku
-    const result = await print(PROGRAMS.SUDOKU);
-    return result;
-  }
+  const handleSubmit = async (event) => {
+    // prevent the browser from reloading the page
+    event.preventDefault();
+    const form = event.target;
+    const formData = new FormData(form);
+    const formJson = Object.fromEntries(formData.entries());
+
+    // send data to print function
+    const result = await print(PROGRAMS.SUDOKU, true, formJson);
+    console.log(result);
+  };
 
   return (
     <>
-      <button onClick={printSudoku}>Print sudoku</button>
+      <form onSubmit={handleSubmit}>
+        <p>
+          difficulty
+          <br />
+          <label><input type="radio" name="difficulty" value="easy" /> easy </label>
+          <br />
+          <label><input type="radio" name="difficulty" value="medium" /> medium </label>
+          <br />
+          <label><input type="radio" name="difficulty" value="hard" /> hard </label>
+        </p>
+        <button type="submit">Print</button>
+      </form>
     </>
   );
 }
